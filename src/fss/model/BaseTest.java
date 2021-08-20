13df33719017cc39d2d3936e8e90925938b68a16
Roman Demin <sporco1988@gmail.com>
2021-08-20 07:46:14 +0700

    Add MeetTest class

--- src/fss/model/BaseTest.java
+++ src/fss/model/BaseTest.java
@@ -6,7 +6,7 @@ public class BaseTest {
     protected ArrayList<SimpleTeam> generateTeams(int cnt) {
         var teams = new ArrayList<SimpleTeam>();
         for(int i = 0; i < cnt; ++i) {
-            teams.add(new ClubTeam(i, String.format("%d", i), String.format("%d", i), SimpleTeam.WorldPart.EUROPE, 30, 30, 30));
+            teams.add(new ClubTeam(i, String.format("Team %d", i), String.format("%d", i), SimpleTeam.WorldPart.EUROPE, 30, 30, 30));
         }
         return teams;
     }
---
+++ src/fss/model/MeetTest.java
@@ -0,0 +1,39 @@
+package fss.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MeetTest extends BaseTest {
+    @Test
+    public void meetCalc() {
+        Assert.assertTrue(checkMeetIsCalculated(createMeetAndCalc(false)));
+        Assert.assertTrue(checkMeetIsCalculated(createMeetAndCalc(true)));
+    }
+
+    @Test
+    public void meetNotCalc() {
+        var meet = createMeet();
+        Assert.assertFalse(meet.isAlreadyCalculated());
+    }
+
+    private Meet createMeet() {
+        var teams = generateTeams(2);
+        return new Meet(teams.get(0), teams.get(1));
+    }
+
+    private Meet createMeetAndCalc(boolean useOwner) {
+        var meet = createMeet();
+        if(useOwner) {
+            meet.calcUseOwner();
+        } else {
+            meet.calc();
+        }
+        return meet;
+    }
+
+    private boolean checkMeetIsCalculated(Meet meet) {
+        return  meet.isAlreadyCalculated()
+                && meet.getResultMeet().getGoalHome() >= 0
+                && meet.getResultMeet().getGoalAway() >= 0;
+    }
+}
