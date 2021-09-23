23f2cd172115b33c3747771e59c5221a4b0cf35c
Roman Demin <sporco1988@gmail.com>
2021-09-23 07:29:07 +0700

    Added GroupsStagePool class, added toString function to all implements of StagePool classes

--- src/fss/model/RoundRobinStagePool.java
+++ src/fss/model/RoundRobinStagePool.java
@@ -43,6 +43,16 @@ class RoundRobinStagePool extends BaseRoundRobinStagePool {
     public ArrayList<SimpleTeam> getLastN(int cnt) {
         return new ArrayList<>(stage.getNLast(cnt));
     }
+
+    @Override
+    public String toString() {
+        var sb = new StringBuilder();
+        sb.append("Round-robin stage: " + name);
+        sb.append(System.lineSeparator());
+        sb.append(stage.toString());
+        sb.append(System.lineSeparator());
+        return sb.toString();
+    }
 }
 
 class PlayOffStagePool extends BasePlayOffStagePool {
@@ -69,159 +79,79 @@ class PlayOffStagePool extends BasePlayOffStagePool {
     public ArrayList<SimpleTeam> getLosers() {
         return stage.getLosers();
     }
-}
-
-/*
-
-public class RoundRobinStagePool1 extends BaseRoundRobinStagePool {
-    private ArrayList<Stage> stages = new ArrayList<>();
-    private ArrayList<Table.WinRules> rules = null;
-
-    public RoundRobinStagePool(String name, ArrayList<SimpleTeam> teams, Rating rating, int cntRounds) {
-        super(name, teams, rating, cntRounds);
-    }
-
-    */
-/*public RoundRobinStagePool(String name, int cntGroups, ArrayList<SimpleTeam> teams, Rating rating, int cntRounds) {
-        super(name, cntGroups, teams, rating, cntRounds);
-    }*//*
-
 
     @Override
-    public void addWinRules(ArrayList<Table.WinRules> rules) {
-        this.rules = rules;
+    public String toString() {
+        var sb = new StringBuilder();
+        sb.append("Play off stage: " + name);
+        sb.append(System.lineSeparator());
+        sb.append(stage.toString());
+        sb.append(System.lineSeparator());
+        return sb.toString();
+    }
+}
+
+class GroupsStagePool extends BaseGroupsStagePool {
+    private ArrayList<CircleStage> stages = null;
+
+    public GroupsStagePool(String name, int cntGroups, ArrayList<SimpleTeam> teams, Rating rating, int cntRounds) {
+        super(name, cntGroups, teams, rating, cntRounds);
     }
 
     @Override
     public void calc() {
         createStages();
-
         for(var s : stages) {
             s.calc();
         }
     }
 
+    private void createStages() {
+        var groups = Sortition.groupSort(teams, cntGroups, rating);
+        for(int i = 0; i < groups.size(); ++i) {
+            var stage = new CircleStage(name + ". Group " + Integer.toString(i + 1), cntRounds);
+            stage.addWinRules(rules);
+            stage.addTeams(groups.get(i));
+            stages.add(stage);
+        }
+    }
+
     @Override
     public ArrayList<SimpleTeam> getWinners() {
-        var result = new ArrayList<SimpleTeam>();
+        var res = new ArrayList<SimpleTeam>();
         for(var stage : stages) {
-            switch (stageType) {
-                case PLAYOFF: {
-                    result.addAll(((PlayOffStage)stage).getWinners());
-                    break;
-                }
-                case GROUPS:
-                case CIRCLE: {
-                    result.addAll(((CircleStage)stage).getNFirst(1));
-                    break;
-                }
-            }
+            res.addAll(stage.getNFirst(1));
         }
-        return result;
+        return res;
     }
 
     @Override
     public ArrayList<SimpleTeam> getLosers() {
-        var result = new ArrayList<SimpleTeam>();
+        var res = new ArrayList<SimpleTeam>();
         for(var stage : stages) {
-            switch (stageType) {
-                case PLAYOFF: {
-                    result.addAll(((PlayOffStage)stage).getLosers());
-                    break;
-                }
-                case GROUPS:
-                case CIRCLE: {
-                    result.addAll(((CircleStage)stage).getNLast(1));
-                    break;
-                }
-            }
+            res.addAll(stage.getNLast(1));
         }
-        return result;
-    }
-
-    @Override
-    public ArrayList<SimpleTeam> getFirstN(int cnt) {
-        var result = new ArrayList<SimpleTeam>();
-        for(var stage : stages) {
-            switch (stageType) {
-                case GROUPS:
-                case CIRCLE: {
-                    result.addAll(((CircleStage)stage).getNFirst(cnt));
-                    break;
-                }
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public ArrayList<SimpleTeam> getLastN(int cnt) {
-        var result = new ArrayList<SimpleTeam>();
-        for(var stage : stages) {
-            switch (stageType) {
-                case GROUPS:
-                case CIRCLE: {
-                    result.addAll(((CircleStage)stage).getNLast(cnt));
-                    break;
-                }
-            }
-        }
-        return result;
+        return res;
     }
 
     @Override
     public ArrayList<SimpleTeam> getN(int n) {
-        var result = new ArrayList<SimpleTeam>();
+        var res = new ArrayList<SimpleTeam>();
         for(var stage : stages) {
-            if(stageType == StageType.CIRCLE || stageType == StageType.GROUPS) {
-                var team = ((CircleStage)stage).getNTeam(n);
-                if(team != null) {
-                    result.add(team);
-                }
-            }
+            res.add(stage.getNTeam(n));
         }
-        return result;
+        return res;
     }
 
     @Override
     public String toString() {
         var sb = new StringBuilder();
+        sb.append("Group stage: " + name);
         for(var stage : stages) {
-            sb.append(name);
             sb.append(System.lineSeparator());
             sb.append(stage.toString());
             sb.append(System.lineSeparator());
         }
         return sb.toString();
     }
-
-    private void createStages() {
-        switch (stageType) {
-            case CIRCLE: {
-                var stage = new CircleStage(name, cntRounds);
-                stage.addWinRules(rules);
-                stage.addTeams(teams);
-                stages.add(stage);
-                break;
-            }
-            case PLAYOFF: {
-                var stage = new PlayOffStage(name, cntRounds != 1);
-                stage.addTeams(teams);
-                stage.addMeets(Sortition.playOffSort(teams, rating, cntRounds != 1));
-                stages.add(stage);
-                break;
-            }
-            case GROUPS: {
-                var groups = Sortition.groupSort(teams, cntGroups, rating);
-                for(int i = 0; i < groups.size(); ++i) {
-                    var stage = new CircleStage(name + ". Group " + Integer.toString(i + 1), cntRounds);
-                    stage.addWinRules(rules);
-                    stage.addTeams(groups.get(i));
-                    stages.add(stage);
-                }
-                break;
-            }
-        }
-    }
 }
-*/
