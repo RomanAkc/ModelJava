2ead1f2e78344972ef321d226a02824eb4a99675
Roman Demin <sporco1988@gmail.com>
2021-11-07 08:58:00 +0700

    Created data for tests

--- src/fss/model/UEFARatingCalculatorTest.java
+++ src/fss/model/UEFARatingCalculatorTest.java
@@ -7,6 +7,19 @@ import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
+/*         LC Q1 Q2 LE Q1 Q2
+1  Spain   2  1     1
+2  England 2  1     1     1
+3  Italy   2     1  1
+4  Germany 1        1  1
+5  France  1        1  1
+6  Russia  1        1    1
+7  Portuga 1     1  1
+8  Scotlan 1     1  1
+9  Netherl 1     1  1     1
+10 Austria 0        1     1
+*/
+
 public class UEFARatingCalculatorTest extends BaseTest {
     private static int FIRST_LC_QUALIFICATION          = 1;
     private static int SECOND_LC_QUALIFICATION         = 2;
@@ -21,21 +34,46 @@ public class UEFARatingCalculatorTest extends BaseTest {
     private static int EC_FINAL                        = 11;
 
     private HashMap<String, Country> countries = new HashMap<>();
+    private HashMap<String, ClubTeam> teams = new HashMap<>();
 
     public UEFARatingCalculatorTest() {
-        countries.put("Italy", new Country(1, "Italy", WorldPart.EUROPE));
-        //countries.put("Italy", new Country(1, "Italy", WorldPart.EUROPE));
-        //countries.put("Italy", new Country(1, "Italy", WorldPart.EUROPE));
-        //countries.put("Italy", new Country(1, "Italy", WorldPart.EUROPE));
-        //countries.put("Italy", new Country(1, "Italy", WorldPart.EUROPE));
-        //countries.put("Italy", new Country(1, "Italy", WorldPart.EUROPE));
-        //countries.put("Italy", new Country(1, "Italy", WorldPart.EUROPE));
-        //countries.put("Italy", new Country(1, "Italy", WorldPart.EUROPE));
+        countries.put("Spain", new Country(1, "Spain", WorldPart.EUROPE));
+        countries.put("England", new Country(2, "England", WorldPart.EUROPE));
+        countries.put("Italy", new Country(3, "Italy", WorldPart.EUROPE));
+        countries.put("Germany", new Country(4, "Germany", WorldPart.EUROPE));
+        countries.put("France", new Country(5, "France", WorldPart.EUROPE));
+        countries.put("Russia", new Country(6, "Russia", WorldPart.EUROPE));
+        countries.put("Portugal", new Country(7, "Portugal", WorldPart.EUROPE));
+        countries.put("Scotland", new Country(8, "Scotland", WorldPart.EUROPE));
+        countries.put("Netherlands", new Country(9, "Netherlands", WorldPart.EUROPE));
+        countries.put("Austria", new Country(10, "Austria", WorldPart.EUROPE));
+
+        teams.put("Real Madrid", new ClubTeam(1, "Real Madrid", countries.get("Spain"), 30));
+        teams.put("Barcelona", new ClubTeam(2, "Barcelona", countries.get("Spain"), 30));
+        teams.put("Atletico Madrid", new ClubTeam(3, "Atletico Madrid", countries.get("Spain"), 30));
+        teams.put("Chelsea", new ClubTeam(4, "Chelsea", countries.get("England"), 30));
+        teams.put("Manchester City", new ClubTeam(5, "Manchester City", countries.get("England"), 30));
+        teams.put("Tottenham Hotspur", new ClubTeam(6, "Tottenham Hotspur", countries.get("England"), 30));
+        teams.put("Internazionale", new ClubTeam(7, "Internazionale", countries.get("Italy"), 30));
+        teams.put("Juventus", new ClubTeam(8, "Juventus", countries.get("Italy"), 30));
+        teams.put("Fiorentina", new ClubTeam(9, "Fiorentina", countries.get("Italy"), 30));
+        teams.put("Bayern Munich", new ClubTeam(10, "Bayern Munich", countries.get("Germany"), 30));
+        teams.put("Bremen", new ClubTeam(11, "Bremen", countries.get("Germany"), 30));
+        teams.put("PSG", new ClubTeam(12, "PSG", countries.get("France"), 30));
+        teams.put("Lyon", new ClubTeam(13, "Lyon", countries.get("France"), 30));
+        teams.put("Spartak Moscow", new ClubTeam(14, "Spartak Moscow", countries.get("Russia"), 30));
+        teams.put("Zenit St Petersburg", new ClubTeam(15, "Zenit St Petersburg", countries.get("Russia"), 30));
+        teams.put("Porto", new ClubTeam(16, "Porto", countries.get("Portugal"), 30));
+        teams.put("Benfica", new ClubTeam(17, "Benfica", countries.get("Portugal"), 30));
+        teams.put("Rangers", new ClubTeam(18, "Rangers", countries.get("Scotland"), 30));
+        teams.put("Celtic", new ClubTeam(19, "Celtic", countries.get("Scotland"), 30));
+        teams.put("PSV", new ClubTeam(20, "PSV", countries.get("Netherlands"), 30));
+        teams.put("Willem II", new ClubTeam(21, "Willem II", countries.get("Netherlands"), 30));
+        teams.put("Sturm", new ClubTeam(22, "Sturm", countries.get("Austria"), 30));
     }
 
     @Test
     public void CalculatorTest() {
-
         UEFATournamentTest LC = getLCTournament();
         UEFATournamentTest LE = getLETournament(LC);
 
@@ -50,7 +88,7 @@ public class UEFARatingCalculatorTest extends BaseTest {
         UEFATournamentTest LC = new UEFATournamentTest("LC");
 
         ArrayList<SimpleTeam> teamsQual1 = new ArrayList<>();
-        //teamsQual1.add(new ClubTeam())
+       // teamsQual1.add(new ClubTeam(1, ""))
         var Qual1 = new UEFAStagePoolTest( StageType.PLAYOFF, "LC QUAL 1", teamsQual1, null, 2);
         LC.stages.add(Qual1);
 
