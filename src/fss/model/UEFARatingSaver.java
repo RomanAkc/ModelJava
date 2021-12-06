ac35da4fb1b2ad67d1d359c6c55c46e9fa5a51d1
RomanAkc <sporco1988@gmail.com>
2021-12-06 10:23:50 +0700

    Added ctor and Save method to UEFARatingSaver

--- src/fss/model/UEFARatingSaver.java
+++ src/fss/model/UEFARatingSaver.java
@@ -1,6 +1,16 @@
 package fss.model;
 
 public class UEFARatingSaver implements RatingSaveable {
+    UEFARating rating = null;
+
+    public UEFARatingSaver(UEFARating rating) {
+        this.rating = rating;
+    }
+
+    public boolean Save() {
+        return SaveRating(rating);
+    }
+
     @Override
     public boolean SaveRating(Ratingable rating) {
         return false;
