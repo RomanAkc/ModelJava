7836b258027ee8eb1ff4e0754c74668acd11531d
Roman Demin <sporco1988@gmail.com>
2021-12-16 08:06:49 +0700

    Added Serializable interface to UEFARatingData

--- src/fss/model/UEFARatingData.java
+++ src/fss/model/UEFARatingData.java
@@ -1,19 +1,36 @@
 package fss.model;
 
-class UEFARatingData {
+import java.io.*;
+import java.util.HashMap;
+
+class UEFARatingData implements Serializable {
     public int year = 0;
-    public ClubTeam team = null;
-    public Country country = null;
+    public transient ClubTeam team = null;
+    public transient Country country = null;
     public double point = 0.0;
+    private int teamID = -1;
+    private int countryID = -1;
 
     public UEFARatingData(int year, Country country, double point) {
         this.country = country;
+        countryID = country.getID();
         setInitData(year, point);
     }
 
     public UEFARatingData(int year, ClubTeam team, double point) {
         this.team = team;
+        teamID = team.getID();
         setInitData(year, point);
+    }
+
+    public void RestoreClubCountry(HashMap<Integer, ClubTeam> teams, HashMap<Integer, Country> countries) {
+        if(teams.containsKey(teamID)) {
+            team = teams.get(teamID);
+        }
+
+        if(countries.containsKey(countryID)) {
+            country = countries.get(countryID);
+        }
     }
 
     private void setInitData(int year, double point) {
