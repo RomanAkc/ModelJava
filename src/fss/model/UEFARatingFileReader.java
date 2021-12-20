07482727cb3bc54d16212be96fc01e5fdb98d810
RomanAkc <sporco1988@gmail.com>
2021-12-20 08:15:59 +0700

    Wrote reading UEFARating from file

--- src/fss/model/UEFARatingFileReader.java
+++ src/fss/model/UEFARatingFileReader.java
@@ -1,8 +1,33 @@
 package fss.model;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
 public class UEFARatingFileReader implements RatingReadable {
+    private FileInputStream fileStream = null;
+    public UEFARatingFileReader(FileInputStream fileStream) {
+        this.fileStream = fileStream;
+    }
+
     @Override
     public Ratingable ReadRating() {
-        return null;
+        ArrayList<UEFARatingData> data = new ArrayList<>();
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileStream);
+            int cnt = objectInputStream.readInt();
+            for(int i = 0; i < cnt; ++i) {
+                UEFARatingData obj = (UEFARatingData) objectInputStream.readObject();
+                data.add(obj);
+            }
+
+        } catch (IOException | ClassNotFoundException e) {
+            return null;
+        }
+
+        UEFARating rating = new UEFARating(data);
+        return rating;
     }
 }
