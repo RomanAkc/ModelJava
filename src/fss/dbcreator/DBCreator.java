cd7eb4390e602335244beee0c35565596df56c38
RomanAkc <sporco1988@gmail.com>
2022-02-01 08:43:42 +0700

    Started to trying reading DataBaseTemplate.xml

--- src/fss/dbcreator/DBCreator.java
+++ src/fss/dbcreator/DBCreator.java
@@ -1,4 +1,12 @@
 package fss.dbcreator;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.sql.*;
@@ -13,6 +21,8 @@ public class DBCreator {
 
     public static void main(String[] args) {
         try {
+            ReadXML();
+
             Class.forName(DB_Driver);
             Properties props = new Properties();
             props.setProperty("user", args[0]);
@@ -31,6 +41,45 @@ public class DBCreator {
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("SQL Error!");
+        }
+    }
+
+    private static void ReadXML() {
+        try {
+            // Создается построитель документа
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            // Создается дерево DOM документа из файла
+            Document document = documentBuilder.parse("DataBaseTemplate.xml");
+
+            // Получаем корневой элемент
+            Node root = document.getDocumentElement();
+
+            /*System.out.println("List of books:");
+            System.out.println();
+            // Просматриваем все подэлементы корневого - т.е. книги
+            NodeList books = root.getChildNodes();
+            for (int i = 0; i < books.getLength(); i++) {
+                Node book = books.item(i);
+                // Если нода не текст, то это книга - заходим внутрь
+                if (book.getNodeType() != Node.TEXT_NODE) {
+                    NodeList bookProps = book.getChildNodes();
+                    for(int j = 0; j < bookProps.getLength(); j++) {
+                        Node bookProp = bookProps.item(j);
+                        // Если нода не текст, то это один из параметров книги - печатаем
+                        if (bookProp.getNodeType() != Node.TEXT_NODE) {
+                            System.out.println(bookProp.getNodeName() + ":" + bookProp.getChildNodes().item(0).getTextContent());
+                        }
+                    }
+                    System.out.println("===========>>>>");
+                }
+            }*/
+
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace(System.out);
+        } catch (SAXException ex) {
+            ex.printStackTrace(System.out);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
         }
     }
 
